d0e29ea2ea32dd1c783d6aa45c9b9d0c4f3f9131
José De Lira <152633410+a01569171@users.noreply.github.com>
2025-04-25 15:49:12 +0000

    tt

--- src/components/ActualizarPersonaje.js
+++ src/components/ActualizarPersonaje.js
@@ -63,6 +63,11 @@ function ActualizarPersonaje() {
                         <label>Email:</label>
                         <input type="email" name="email" value={personaje.email || ''} onChange={handleInputChange} required />
                     </div>
+
+                    <div>
+                        <label>whatsapp:</label>
+                        <input type="whatsapp" name="whatsapp" value={personaje.whatsapp || ''} onChange={handleInputChange} required />
+                    </div>
                     <button type="submit">Actualizar Personaje</button>
                 </form>
             )}
